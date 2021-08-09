804ec9ae841fb81510c037145b89f8b61f19343f
paurush2508 <psrivastava@dealersocket.com>
2021-08-09 23:30:56 +0530

    frontend changes complete

---
+++ components/Footer.js
@@ -0,0 +1,40 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 text-gray px-32 py-14 bg-gray-100">
+      <div className="text-xs space-y-4 text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="text-xs space-y-4 text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It's a pretty awesome clone</p>
+        <p>Referrals accepted</p>
+        <p>Papafam</p>
+      </div>
+      <div className="text-xs space-y-4 text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Papa React</p>
+        <p>Presents</p>
+        <p>Zero to Fullstack here</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+      <div className="text-xs space-y-4 text-gray-800">
+        <h5 className="font-bold">SUPPORT</h5>
+        <p>How Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
